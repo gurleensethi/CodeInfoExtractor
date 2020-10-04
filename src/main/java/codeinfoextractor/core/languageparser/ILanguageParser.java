d50b4d277d8ca488e04dee8eb40c67cb777f54be
Gurleen Sethi <sarusethi@rocketmail.com>
2020-10-04 12:23:11 -0400

    doc: add javadoc for ILanguageParser and its method.

--- src/main/java/codeinfoextractor/core/languageparser/ILanguageParser.java
+++ src/main/java/codeinfoextractor/core/languageparser/ILanguageParser.java
@@ -2,6 +2,19 @@ package codeinfoextractor.core.languageparser;
 
 import codeinfoextractor.core.models.LanguageParseResult;
 
+/**
+ * Contract to be satisfied by a parser.
+ * A parser will receive code data in the form a long string.
+ *
+ * Once implemented, the parser should be registered with
+ * an instance of InfoExtractor.
+ */
 public interface ILanguageParser {
+    /**
+     * Should contain the parsing logic for a language.
+     *
+     * @param data source code data to be parsed.
+     * @return result of parsing.
+     */
     LanguageParseResult parse(String data);
 }
